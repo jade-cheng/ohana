19bdc6fe3a2fbaa04b441466d1115d2bb7dd971e
Jade Cheng <info@jade-cheng.com>
2016-05-27 17:52:04 +0200

    Initialize F to Gaussian random values of sigma 0.01

--- src/lib/jade.version.hpp
+++ src/lib/jade.version.hpp
@@ -30,7 +30,7 @@ namespace jade
                 char_type const * const title, ///< The program name.
                 ostream_type &          out)   ///< The output stream.
         {
-            static const values current { 0, 0, 5985, 36993, 2015, 2016 };
+            static const values current { 0, 0, 5991, 32134, 2015, 2016 };
 
             out << "ohana/" << title << ' '
                 << current.major << '.'
